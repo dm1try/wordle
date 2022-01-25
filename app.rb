ea3e7cd68988f248f7d570e317304e359130a763
dm1try <me@dmitry.it>
2022-01-25 22:59:20 +0300

    mv method from global namespace

--- app.rb
+++ app.rb
@@ -6,6 +6,13 @@ class App
     end
   end
 
+  def self.ws_handler(conn)
+    while msg = JSON.parse(conn.recv)
+      puts "Received: #{msg}"
+      conn.send msg
+    end
+  end
+
   def call(env)
     [200, {'Content-Type' => 'text/html'}, ['Game']]
   end
--- server.rb
+++ server.rb
@@ -1,18 +1,11 @@
 app_path = File.expand_path('./config.ru', __dir__)
 app = Tipi::RackAdapter.load(app_path)
 
-def ws_handler(conn)
-  while msg = conn.recv
-    puts "Received: #{msg}"
-    conn.send msg
-  end
-end
-
 opts = {
   reuse_addr:  true,
   dont_linger: true,
   upgrade:     {
-    websocket: Tipi::Websocket.handler(&method(:ws_handler))
+    websocket: Tipi::Websocket.handler(&App.method(:ws_handler))
   }
 }
 
