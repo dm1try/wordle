8d3e69c6a7db2a9a4f7782946f5cf3f8a91f07b9
dm1try <me@dmitry.it>
2022-02-01 20:56:07 +0300

    setup script

--- setup/prepare_test_db.rb
+++ setup/prepare_test_db.rb
@@ -2,6 +2,6 @@ require_relative '../db'
 
 if ENV['APP_ENV'] == 'test'
   $redis.flushdb
-  $redis.sadd('words', 'plain')
-  $redis.sadd('available_words', 'plain')
+  $redis.sadd('words_en', 'plain')
+  $redis.sadd('available_words_en', 'plain')
 end
