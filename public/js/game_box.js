49ca4733d50904100a9832d8cc03e48a12703e2d
dm1try <me@dmitry.it>
2022-02-06 19:23:54 +0300

    allow notifications with buttons closes #28

--- public/js/game_box.js
+++ public/js/game_box.js
@@ -1,14 +1,36 @@
 class NotifyBox extends React.Component {
   render(){
+    var content = null;
+
+    if (typeof(this.props.notify) == 'object') {
+      var button_bg_color = this.props.notify.button.bg_color || 'green';
+
+      content = React.createElement('div', {},
+        React.createElement('span', {
+          className: "block sm:inline"
+        },
+          this.props.notify.message
+        ),
+        React.createElement('button', {
+          onClick: this.props.notify.button.onClick,
+          className: "bg-" + button_bg_color + "-500 hover:bg-" + button_bg_color + "-700 text-white font-bold px-2 mx-1 rounded"
+        },
+          this.props.notify.button.text
+        )
+      )
+    }else{
+      content = React.createElement('span', {
+        className: "block sm:inline"
+      },
+        this.props.notify
+      )
+    }
+
     return React.createElement('div', {
       className: "m-2 border px-4 py-3 rounded relative text-center",
       role: "alert"
     },
-      React.createElement('span', {
-        className: "block sm:inline"
-      },
-        this.props.message ||  "Loading..."
-      )
+      content
     );
   }
 }
@@ -151,7 +173,7 @@ class GameBox extends React.Component {
         onClick: this.props.onClick
       }),
       React.createElement(NotifyBox, {
-        message: this.props.notify_message || this.statusDescription()
+        notify: this.props.notify || this.statusDescription()
       }),
       React.createElement(Keyboard, {
         rows: this.keyboardLayout(),
